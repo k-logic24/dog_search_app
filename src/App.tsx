74111bb847fd6a5356214d8edbe20b98488191a8
岩田康平 <iwater0715@gmail.com>
2020-11-03 11:30:55 +0900

    微調整

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 library.add(faChevronRight, faChevronLeft)
 
-
 type State = {
   dogs: string[]
   loading: boolean
