ed23a58d4b7f8b4ff65071ba37e0833e9bea4583
岩田康平 <iwater0715@gmail.com>
2020-11-03 09:48:13 +0900

    contextの準備段階

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import React, { FC } from 'react'
+import React, { FC, useReducer, createContext } from 'react'
+
 import './style/tailwind.output.css'
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
 
+type State = {
+  dogs: string[]
+  loading: boolean
+}
+
+type Action = {
+  type: 'FETCH_DOG'
+  payload: any
+}
+
+const initialState = {
+  dogs: [],
+  loading: false
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'FETCH_DOG':
+      return { ...state, ...action.payload }
+    default:
+      return state
+  }
+}
+
+const DogContext = createContext({} as {
+  state: State
+  dispatch: React.Dispatch<Action>
+})
+
 const App: FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <>
+    <DogContext.Provider value={{ state, dispatch }}>
       <Header />
       <Footer />
-    </>
+    </DogContext.Provider>
   )
 }
 
--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,9 +3,7 @@ import React, { FC } from 'react'
 const Header: FC = () => {
   return (
     <footer className="p-4 bg-blue-500 text-center">
-      <small className="text-white">
-        &copy;2020 Iwata
-      </small>
+      <small className="text-white">&copy;2020 Iwata</small>
     </footer>
   )
 }
